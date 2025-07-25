6a21db6406fd37b298d1af40d083419ffaef99ed
Toi31024 <itoeat1101.1996@gmail.com>
2025-07-25 22:12:26 +0900

    playerController

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -5,12 +5,27 @@ public class player_controller : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.Translate(0, 3, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Translate(3, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.Translate(0, -3, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Translate(-3, 0, 0);
+        }
     }
 }
