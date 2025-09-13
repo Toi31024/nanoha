5417e1bc4368b3f0dce050c1dc35b80f51189270
s1f102500731 <s1f102500731@iniad.org>
2025-09-13 10:22:15 +0900

    test

--- Assets/scripts/traveller.cs
+++ Assets/scripts/traveller.cs
@@ -4,7 +4,6 @@ public class traveller : MonoBehaviour
 {
     private Animator anim = null;
     private Traveller_controller traveller_InputAction;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         traveller_InputAction = new Traveller_controller();
@@ -12,7 +11,6 @@ public class traveller : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         //移動モーション
