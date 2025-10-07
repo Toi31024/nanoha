590a558b904b21fe2cac398ba5413466f188f25c
Toi31024 <itoeat1101.1996@gmail.com>
2025-10-07 10:40:14 +0900

    you died

--- Assets/scripts/system/GameManagerScript.cs
+++ Assets/scripts/system/GameManagerScript.cs
@@ -4,14 +4,11 @@ public class GameManagerScript : MonoBehaviour
 {
     [SerializeField] player1 player1;
     [SerializeField] player2 player2;
-    SmashBall smashBall;
     GameObject obj;
 
     void Start()
     {
         obj = GameObject.Find("target1_0");
-        smashBall = obj.GetComponent<SmashBall>();
-        smashBall.smashball_spawn();
     }
 
     void Update()
