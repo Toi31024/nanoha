2cc6468a637172609684aee444132787e481a930
s1f102500731 <s1f102500731@iniad.org>
2025-10-31 01:31:53 +0900

    ビーム見た目だけ完成

--- Assets/texture/baem/script/cahinsaw.cs
+++ Assets/texture/baem/script/cahinsaw.cs
@@ -4,8 +4,11 @@ public class cahinsaw : MonoBehaviour
 {
     [SerializeField] private Animator anim;
     [SerializeField] private GameObject beam_obj;
+    [SerializeField] private GameObject alert;
     private float cur_time = 0;
     private float pos_y;
+
+    private int how_many_times = 0;
 
     private bool setup_bool = true;
     private float setup_sec = 2.0f;
@@ -24,13 +27,14 @@ public class cahinsaw : MonoBehaviour
     {
         cur_time += Time.deltaTime;
 
+        //ビームガンの位置を決定
         if (cur_time >= setup_sec && setup_bool == true)
         {
             cur_time = 0;
-            Debug.Log("ビーム開始");
             pos_y = UnityEngine.Random.Range(-4.0f, 12.0f);
             this.transform.localPosition = new Vector3(-12.0f, pos_y, -3.0f);
-            beam_obj.transform.position = new Vector3(0, pos_y - 3.5f, -3.1f);
+            beam_obj.transform.position = new Vector3(2.2f, pos_y - 3.4f, -3.1f);
+            alert.transform.position = new Vector3(0, pos_y - 3.885f, -2.9f);
 
             setup_bool = false;
             spin_bool = true;
@@ -39,6 +43,8 @@ public class cahinsaw : MonoBehaviour
         //普通のスピン遷移
         if (cur_time >= spin_sec && spin_bool == true)
         {
+            alert.SetActive(true);
+
             anim.SetBool("spin", true);
             anim.SetBool("superspin", false);
             anim.SetBool("wait", false);
@@ -65,7 +71,7 @@ public class cahinsaw : MonoBehaviour
         //攻撃遷移
         if (cur_time >= attack_sec && attack_bool == true)
         {
-            //攻撃のスクリプト書く
+            alert.SetActive(false);
             beam_obj.SetActive(true);
             if (cur_time >= attack_end_sec)
             {
@@ -79,6 +85,26 @@ public class cahinsaw : MonoBehaviour
                 setup_bool = true;
 
                 cur_time = 0;
+
+                //時間がたつほど、ビームの頻度が増えるようにする(難易度増加)
+                how_many_times++;
+                if (how_many_times >= 3 && how_many_times < 7)
+                {
+                    Debug.Log("難易度増加1");
+                    setup_sec = 1.5f;
+                    super_spin_sec = 1.0f;
+                    attack_sec = 1.2f;
+                    attack_end_sec = 1.75f;
+                }
+                if (how_many_times >= 7 && how_many_times < 14)
+                {
+                    Debug.Log("難易度増加2");
+                    setup_sec = 1.0f;
+                    spin_sec = 0.25f;
+                    super_spin_sec = 0.75f;
+                    attack_sec = 0.9f;
+                    attack_end_sec = 1.45f;
+                }
             }
         }
     }
