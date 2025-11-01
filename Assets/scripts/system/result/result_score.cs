ed16193674473d836bb6bef2e3e05dc95b7defdd
s1f102500731 <s1f102500731@iniad.org>
2025-11-01 22:12:34 +0900

    リザルト　文字とインド人を左に

--- Assets/scripts/system/result/result_score.cs
+++ Assets/scripts/system/result/result_score.cs
@@ -13,7 +13,7 @@ public class Result_score : MonoBehaviour
     void Start()
     {
         // ScoreManagerから最終スコアを読み出し、テキストに表示する
-        score_txt.text = "Your score is " + ScoreManager.finalScore;
+        score_txt.text = "Your score : " + ScoreManager.finalScore;
 
         // ★★★ ScoreManagerから破壊したターゲット数を読み出し、テキストに表示する処理を追加 ★★★
         targetsDestroyed_txt.text = "Targets Destroyed: " + ScoreManager.targetsDestroyed;
