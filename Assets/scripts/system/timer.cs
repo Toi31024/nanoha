15dd5d8a5f8140f70ab8cc7955a05e6e512a6a65
s1f102500731 <s1f102500731@iniad.org>
2025-11-03 09:50:50 +0900

    タイマー途中まで

--- Assets/scripts/system/timer.cs
+++ Assets/scripts/system/timer.cs
@@ -6,14 +6,17 @@ public class timer : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI timer_text;
     public static float time = 0;
+
+    private float timer_senyou_time;
     void Start()
     {
-
+        timer_senyou_time = 0;
     }
 
     void Update()
     {
         time += Time.deltaTime;
-        timer_text.text = time.ToString("F2");
+        timer_senyou_time += Time.deltaTime;
+        timer_text.text = timer_senyou_time.ToString("F2");
     }
 }
